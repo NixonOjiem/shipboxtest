3bbe68d8fddfd075604ad28928880665b4e09ee5
NixonOjiem <nicksonogm@gmail.com>
2026-03-10 09:19:21 +0300

    modified the product model and included relationship (many to many) with orders

--- server/app/Models/Product.php
+++ server/app/Models/Product.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'price', 'current_stock'];
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class)->withPivot('quantity')->withTimestamps();
+    }
 }
