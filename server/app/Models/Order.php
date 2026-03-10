de8c3f10b1b360f7db3b60a940aa4cbaed035661
NixonOjiem <nicksonogm@gmail.com>
2026-03-10 09:24:33 +0300

    modified the order model to include user relationship and product relationship

--- server/app/Models/Order.php
+++ server/app/Models/Order.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'user_id',
+        'customer_phone',
+        'customer_address',
+        'status',
+        'note',
+        'total_price',
+        'quantity'
+    ];
+
+    public function seller()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class)->withPivot('quantity')->withTimestamps();
+    }
 }
