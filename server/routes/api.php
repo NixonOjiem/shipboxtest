15611d50352d2b43498ac12a12eb727d94f10221
NixonOjiem <nicksonogm@gmail.com>
2026-03-10 08:07:31 +0300

    api working as intended

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/testserver', function () {
+    return response()->json(['messages' => 'Hi server running']);
+});
