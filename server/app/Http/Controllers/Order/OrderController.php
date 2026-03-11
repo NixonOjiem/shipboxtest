01dc7e189d9026132ba1b901e4647b8a3de6014b
NixonOjiem <nicksonogm@gmail.com>
2026-03-11 10:31:26 +0300

    created boilerplate code for ordercontroller

---
+++ server/app/Http/Controllers/Order/OrderController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Order;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Order;
+
+class OrderController extends Controller
+{
+    //To handle order creation event
+    public function createOrder(Request $request)
+    {
+
+    }
+
+    public function fetchOrders(Request $request)
+    {
+
+    }
+
+    public function deleteOrder(Request $request, Order $order)
+    {
+
+    }
+
+    public function updateOrder(Request $request, Order $order)
+    {
+
+    }
+
+}
